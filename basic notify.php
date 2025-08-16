eb076f10e869f178cd5b950abb0d1c1b4fc5f734
Sbrixton <sanelebangani7@gmail.com>
2025-08-17 01:30:27 +0200

    Update basic notify.php

--- basic notify.php
+++ basic notify.php
@@ -1,17 +1,5 @@
 <?php
-$log = "===== New Notify Hit =====\n";
-$log .= "Request Method: " . $_SERVER['REQUEST_METHOD'] . "\n";
-$log .= "Date: " . date('Y-m-d H:i:s') . "\n";
-
-// Raw POST
-$raw_post = file_get_contents('php://input');
-$log .= "Raw POST: " . $raw_post . "\n";
-
-// $_POST
-$log .= "_POST: " . print_r($_POST, true) . "\n";
-
-// Write to notify.log
-file_put_contents('notify.log', $log . "\n", FILE_APPEND);
-
+// Log only basic _POST to a safe file
+file_put_contents('log.txt', print_r($_POST, true), FILE_APPEND);
 echo "OK";
 ?>
