12718ba85a09146219651367c90040e76ca25c1b
Sbrixton <sanelebangani7@gmail.com>
2025-08-17 03:10:13 +0200

    Update basic notify.php

--- basic notify.php
+++ basic notify.php
@@ -1,4 +1,9 @@
 <?php
-file_put_contents('notify.log', json_encode($_POST) . "\n", FILE_APPEND);
+$logPath = __DIR__ . '/notify.log';
+
+file_put_contents($logPath, "Script hit at " . date('Y-m-d H:i:s') . "\n", FILE_APPEND);
+
+file_put_contents($logPath, json_encode($_POST) . "\n", FILE_APPEND);
+
 echo "OK";
 ?>
