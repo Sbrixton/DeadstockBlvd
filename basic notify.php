88a669c06ec2364e136f71a236e315b18eb43463
Sbrixton <sanelebangani7@gmail.com>
2025-08-17 02:53:04 +0200

    Update basic notify.php

--- basic notify.php
+++ basic notify.php
@@ -1,11 +1,5 @@
 <?php
-$logPath = __DIR__ . '/notify.log'; // safer path
-
-$raw_post = file_get_contents('php://input');
-$post_data = $_POST ?: json_decode($raw_post, true);
-
-// Save the IPN data
-file_put_contents($logPath, print_r($post_data, true) . "\n", FILE_APPEND);
+mail("your.email@example.com", "PayFast IPN Hit", "IPN Received");
 
 echo "OK";
 ?>
